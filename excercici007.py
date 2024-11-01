e41c8ee7aec9688246e7cf375cce61c115ad5ede
Alvarjur <aalvarojur@gmail.com>
2024-11-01 15:38:51 +0100

    7

--- excercici007.py
+++ excercici007.py
@@ -49,12 +49,40 @@ def app_events():
 def app_run():
     pass
 
+def draw_polygon(screen, color, center, radius, num_vertices, angle_offset=(math.pi / 3)):
+    points = [
+        (
+            center[0] + radius * math.cos(angle_offset + i * 2 * math.pi / num_vertices),
+            center[1] + radius * math.sin(angle_offset + i * 2 * math.pi / num_vertices)
+        )
+        for i in range(num_vertices)
+    ]
+    pygame.draw.polygon(screen, color, points)
+
 # Dibuixar
 def app_draw():
     screen.fill(WHITE)
     utils.draw_grid(pygame, screen, 50)
-    
-    
+
+    colors = [(127, 184, 68), (240, 187, 64), (226, 137, 50), (202, 73, 65), (135, 65, 152), (75, 154, 217)]
+    greyscale = [0, 0, 0]
+
+    coordsX = 0
+    for q in range(0, len(colors)):
+        pygame.draw.rect(screen, colors[q], ((50 + coordsX, 50), (50, 50)))
+        pygame.draw.circle(screen, colors[q], (75 + coordsX, 175), 25, 2)
+        coordsX += 100
+
+    coordsX = 0
+    for i in range(6):
+        draw_polygon(screen, greyscale, (75 + coordsX, 275), 25, 3)
+        draw_polygon(screen, greyscale, (75 + coordsX, 375), 25, 5)
+        greyscale[0] += 25
+        greyscale[1] += 25
+        greyscale[2] += 25
+        coordsX += 100
+
+
     pygame.display.update()
 
 if __name__ == "__main__":
