ea49c852fa54420ffdd23aa0fe446d7f7331bbdb
Alvarjur <aalvarojur@gmail.com>
2024-11-02 17:44:32 +0100

    16

---
+++ excercici016.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import utils
+
+# Definir colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+PINK = (255, 105, 180)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Definir la finestra
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Window Title')
+
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Botó tancar finestra
+            return False
+    return True
+
+# Fer càlculs
+def app_run():
+    pass
+
+# Dibuixar
+def app_draw():
+    screen.fill(WHITE)
+    utils.draw_grid(pygame, screen, 50)
+
+    rows = 15
+    columns = 21
+
+    hueStep = (360 / columns)
+
+    for row in range(0, rows):
+        for column in range(0, columns):
+            hue = hueStep * column
+
+            color = utils.hsl_to_rgb(hue, 1, (1/15) * row)
+            pygame.draw.rect(screen, color, ((50 + column * 25, 50 + row * 25), (25, 25)))
+
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()
